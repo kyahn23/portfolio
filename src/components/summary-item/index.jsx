32ee321c61b9bd2ff3a6e1b983fe5e07910e7dae
kyahn23 <kyahn23@gmail.com>
2022-11-30 04:22:43 +0900

    test

--- src/components/summary-item/index.jsx
+++ src/components/summary-item/index.jsx
@@ -25,7 +25,7 @@ const SummaryItem = ({ name, description, link = false, internal = false, github
         {link ? linkContent : name}
       </h3>
       <p className={classes.description}>{description}</p>
-      {github ? <p className={classes.description}><a href={github}>{ github}</a></p> : ''}
+      {github ? <p className={classes.description}><a href={github}>{ github}</a></p> : 'asdf'}
     </div>
   );
 };
